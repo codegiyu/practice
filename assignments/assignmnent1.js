1a597bdfb2f602f69a6aa241750680699f101cef
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-10-02 21:58:11 +0100

    initial

---
+++ assignments/assignmnent1.js
@@ -0,0 +1,93 @@
+/// VARIABLES
+
+// 1. Declare variables and assign string, number, boolean, object, undefined and null data types.
+
+// 2. Use the JavaScript typeof operator to check the data type of each variable.
+
+// 3. Declare any two variables without assigning values.
+
+// 4. Declare variables to store your first name, last name, marital status, country and age in a single line.
+
+// 5. Write three JavaScript statement which provide truthy value.
+
+// 6. Write three JavaScript statement which provide falsy value.
+
+
+/// OPERATORS
+
+// 7. Using javascript mathematical operators, perform the following operations with these two numbers, 8 and 3.
+//    You can put any one before the other in your operations.
+// a. Addition
+// b. Subtraction
+// c. Multiplication
+// d. Division
+// e. Remainder
+// f. Power
+
+// 8. Even numbers are divisible by 2 and the remainder is zero. 
+//    How do you check, if a number is even or not using JavaScript?
+
+// 9. Figure out the result of the following comparison expression first without using console.log(). 
+//    After you decide the result confirm it using console.log()
+
+// a. 4 > 3
+// b. 4 >= 3
+// c. 4 < 3
+// d. 4 <= 3
+// e. 4 == 4
+// f. 4 === 4
+// g. 4 != 4
+// h. 4 !== 4
+// i. 4 != '4'
+// j. 4 == '4'
+// k. 4 === '4'
+// l. 4 > 3 && 10 < 12
+// m. 4 > 3 && 10 > 12
+// n. 4 > 3 || 10 < 12
+// o. 4 > 3 || 10 > 12
+// p. !(4 > 3)
+// q. !(4 < 3)
+// r. !(false)
+// s. !(4 > 3 && 10 < 12)
+// t. !(4 > 3 && 10 > 12)
+// u. !(4 === '4')
+
+
+///ACCESSING ARRAY AND OBJECT ITEMS
+
+// 10. Create a variable named myArray with the const keyword and assign an array with at least 5 items to it.
+// a. Use a javascript method to find the length of your array.
+// b. Change the 3rd and 5th items in your array to 'third' and 5
+// c. Use a console log to access the value of the second item.
+
+// 11. Create a variable named myObject with the let keyword and assign an object with the following keys to it.
+//     first name, last name, marital status, country and age. Give them any values of your choice.
+// a. Change the value of your country key to 'Jand'
+
+
+/// CONDITIONALS
+
+// 12. If a is greater than b return 'a is greater than b' else 'a is less than b'. 
+//    Try to implement it in two ways
+// a. Using if else
+
+// b. Using ternary operator
+
+// 13. Write a code which can give grades to students according to theirs scores
+//     You may use whatever method you like
+//     80-100, A
+//     70-89, B
+//     60-69, C
+//     50-59, D
+//     0-49, F
+
+// 14. Check if the season is Autumn, Winter, Spring or Summer. If the user input is...
+//     Use switch statements for this one
+//     September, October or November, the season is Autumn.
+//     December, January or February, the season is Winter.
+//     March, April or May, the season is Spring
+//     June, July or August, the season is Summer
+
+// 15. Check if a day is weekend day or a working day. Your script will take day as an input.
+
+// 16. Write a program which tells the number of days in a month, now consider leap year.
---
+++ lesson-1_01-09-22.js
@@ -0,0 +1,77 @@
+// Numbers
+// Strings
+// Booleans
+// Objects
+// Null
+// Undefined
+// Symbols
+
+// const, let, var
+
+let number = 14557799;
+const string = '5?sjkfl';
+var boolean = true;
+// let object = [{}];
+let e = null;
+let undefined;
+
+console.log(number)
+number = 2
+let stringTwo = 'age red';
+console.log(stringTwo)
+
+let array = [1,2, true, 'eor', ['iyuhk'], {nam: 7}, null]
+array[number] = 7
+console.log(array.length)
+
+const object = {
+    name: stringTwo,
+    'age red': 30
+}
+object['name'] = 'Giyu'
+console.log(object['name']); 
+
+// + - * / % < > = == === <= >= ! != !== **
+let a = 'h'.length
+// a = `My name is ${a + 1}`
+// type coercion && type conversion
+console.log(a)
+
+// false, 0, '', null, undefined, NaN, 
+
+console.log(Number(!0))
+
+let second = [1,2,3,4,5,6]
+true && 1
+// if (second.length > 5 && second[0] ) {
+//     second = [0]
+// } else if (second.length !== 6) {
+//     second = [6]
+// } else {
+//     second = 'else'
+// }
+
+console.log(second)
+
+console.log(0 || '' || false || undefined)
+let month = 'December'
+let season;
+// switch(true) {
+//     case second.length > 5 && second[0]:
+//         second = [0]
+//         break;
+
+//     case second.length !== 6:
+//         second =  [6];
+//         break;
+
+//     default:
+//         second =  'else';
+// }
+
+console.log(second)
+console.log(season);
+
+second = second.length > 5 && second[0] ? [0] : second.length == 6 ? ['six'] : second.push(9)
+// console.log(second.push(4))
+console.log(second)
